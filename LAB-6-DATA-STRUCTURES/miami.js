c880ea3203823d645381d13f66437061299b57dc
vatsall18 <143758536+vatsall18@users.noreply.github.com>
2023-10-26 17:05:45 -0400

    updates

---
+++ LAB-6-DATA-STRUCTURES/miami.js
@@ -0,0 +1,67 @@
+const express = require('express')
+
+//add the view engine 
+const expressHandlebars = require('express-handlebars') 
+
+const app = express()
+
+//Static files or folders are specified before any route
+app.use(express.static(__dirname + '/public'))
+
+//configure our express app to use handlebars
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+
+app.set('view engine','handlebars')
+//ends handlebar configuration
+
+const port = process.env.port || 3000
+//require gallery outside the view because we will use the same in all get requests
+const gallery = require('./data/gallery.json')
+//Routes go before 404 and 500
+app.get('/',(req,res)=>{
+    var data = require('./data/home-data.json')
+    res.render('page',{ data, gallery })
+})
+
+app.get('/victoria',(req,res)=>{
+    var data = require('./data/victoria-data.json')
+    res.render('page',{ data,gallery })
+})
+
+app.get('/streetfood',(req,res)=>{
+    var data = require('./data/streetfood-data.json')
+    res.render('page',{ data,gallery })
+})
+
+app.get('/howrah',(req,res)=>{
+    var data = require('./data/howrah-data.json')
+    res.render('page',{ data,gallery })
+})
+app.get('/durgapuja',(req,res)=>{
+    var data = require('./data/durgapuja-data.json')
+    res.render('page',{ data,gallery })
+})
+//This generates an error because the parameter names don't match
+
+
+//Error handling ->  app.use() basic express route 
+app.use((req,res) => {
+    res.status(404)
+    res.render('404')
+})
+
+//Server Error 500
+app.use((error,req,res,next) => {
+    console.log(error.message)
+    res.status(500)
+    res.render('500') 
+}) 
+
+// setup listener
+app.listen(port,()=>{
+    console.log(`Server started http://localhost:${port}`)
+    //console.log('Server starter http://localhost:'+port)
+    console.log('To close pres Ctrl-C')
+})
